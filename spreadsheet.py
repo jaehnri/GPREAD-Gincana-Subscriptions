0667046026d76e9e59e0708a2094e9438f9a20d0
j <joao.henri.cr@gmail.com>
2019-05-19 01:42:12 -0300

    Almost Ended

--- spreadsheet.py
+++ spreadsheet.py
@@ -14,7 +14,6 @@ subscribers = sheet.get_all_records()
 def phone_model(string):
     return string[0:2] + ' ' + string[2:7] + '-' + string[7:]
 
-
 for sub in range(0,len(subscribers)):
     if ("LOL" in subscribers[sub]['Quais provitchas o senhor irá participar?']):
         pp.pprint(' '.join(subscribers[sub]['Fala o nominho (COMPLETO ) pra mim'].split()[0:3]).ljust(25)[:25])
@@ -22,5 +21,3 @@ for sub in range(0,len(subscribers)):
         pp.pprint(phone_model("".join(str(subscribers[sub]['manda o zap com ddd']).split())))
         
         print()
-
-    
